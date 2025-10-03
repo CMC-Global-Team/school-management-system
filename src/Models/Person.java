f685a2199cae257c4509c8094149c002f01b97e5
pl1201 <152005792+pl1201@users.noreply.github.com>
2025-10-03 17:18:08 +0700

    Thêm class Person

---
+++ src/Models/Person.java
@@ -0,0 +1,27 @@
+package Models;
+
+public class Person {
+    protected String id;
+    protected String name;
+    protected String status;
+
+    public Person(String id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+
+    }
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getStatus() {return status;}
+    public void setStatus(String status) {this.status = status;}
+
+    @Override
+    public String toString() {
+        return id + "," + name + "," + status;
+    }
+}
