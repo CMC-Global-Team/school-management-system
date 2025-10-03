56b2bb7cf2ddb4833c64ceef65c2c8bebbbc1d7d
Tran Thanh <tienthanh6042@gmail.com>
2025-10-03 23:12:14 +0700

    Sửa lỗi chính tả

--- src/Screen/DashBoard.java
+++ src/Screen/DashBoard.java
@@ -6,7 +6,7 @@ public class DashBoard {
             return false;
         }
         try{
-            int i = Integer.parseInt(String.valueOf(n));
+            int i = Integer.parseInt(n);
             if(i<=0 || i>=8){
                 return false;
             }
@@ -17,20 +17,22 @@ public class DashBoard {
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("==========================================");
-        System.out.print("============= School Manage ==============");
-        System.out.print("==========================================");
-        System.out.print("[Notice] Nhập 1 trong các số 1-7 để chọn!");
-        System.out.print("==========================================");
-        System.out.print("[1] >> Quản lý Lớp học.");
-        System.out.print("[2] >> Quản lý môn học.");
-        System.out.print("[3] >> Quản lý điểm.");
-        System.out.print("[4] >> Quản lý học sinh.");
-        System.out.print("[5] >> Quản lý giáo viên.");
-        System.out.print("[6] >> Quản lý học phí.");
+        System.out.println("==========================================");
+        System.out.println("============= School Manage ==============");
+        System.out.println("==========================================");
+        System.out.println("[Notice] Nhập 1 trong các số 1-7 để chọn!");
+        System.out.println("==========================================");
+        System.out.println("[1] >> Quản lý Lớp học.");
+        System.out.println("[2] >> Quản lý môn học.");
+        System.out.println("[3] >> Quản lý điểm.");
+        System.out.println("[4] >> Quản lý học sinh.");
+        System.out.println("[5] >> Quản lý giáo viên.");
+        System.out.println("[6] >> Quản lý học phí.");
+        System.out.println("[7] >> Thoát!");
+        System.out.print("Bạn chọn:");
         String option = input.nextLine();
         while(!validate(option)){
-            System.out.print("Lựa chọn không hợp lệ, vui lòng nhập 1 trong các số 1-7:");
+            System.out.print("Lựa chọn không hợp lệ, vui lòng chọn một trong các số 1-7:");
             option = input.nextLine();
         }
     }
