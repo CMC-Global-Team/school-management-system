8831bb8330495ee61ca38a6eb0f26ec53c01e676
pl1201 <152005792+pl1201@users.noreply.github.com>
2025-10-07 21:16:54 +0700

    Tích hợp tự động nhập ID

--- src/Screen/Subject/AddSubjectScreen.java
+++ src/Screen/Subject/AddSubjectScreen.java
@@ -1,18 +1,19 @@
 package Screen.Subject;
 
-
+import Utils.IdGenerator;
 import Screen.AbstractScreen;
 import Services.SubjectService;
 import Models.Subject;
 import Utils.InputUtil;
+
+import java.util.List;
+
 public class AddSubjectScreen extends AbstractScreen {
     private final SubjectService subjectService;
-
 
     public AddSubjectScreen() {
         this.subjectService = SubjectService.getInstance();
     }
-
 
     @Override
     public void display() {
@@ -22,13 +23,20 @@ public class AddSubjectScreen extends AbstractScreen {
         System.out.println("└──────────────────────────────────────────┘");
     }
 
-
     @Override
     public void handleInput() {
         System.out.println("\nNhập thông tin môn học:");
 
 
-        String id = input("Mã Môn Học: ");
+        List<Subject> subjects = subjectService.getAllSubjects(); // lấy toàn bộ môn học
+        List<String> ids = subjects.stream()
+                .map(Subject::getSubjectID)
+                .toList();
+
+        String id = IdGenerator.generateId(ids, "M", 2);
+        System.out.println("Mã môn học gợi ý: " + id);
+        String suggestedId = input("Mã Môn Học (nhấn Enter để dùng gợi ý): ").trim();
+        if (suggestedId.isEmpty()) suggestedId = id;
         String name = input("Tên Môn Học: ");
         int lessonCount = InputUtil.getInt("Số tiết học: ");
         double confficient = InputUtil.getDouble("Hệ số: ");
@@ -37,12 +45,10 @@ public class AddSubjectScreen extends AbstractScreen {
         String teacher = input("Giáo viên phụ trách: ");
         String status = input("Trạng thái (Đang dạy/Ngừng): ");
 
-
         if (subjectService.addSubject(id, name, lessonCount, confficient, type, description, teacher, status)) {
             System.out.println("\nThông tin môn học đã thêm:");
             subjectService.findById(id).ifPresent(System.out::println);
         }
-
 
         pause();
     }
