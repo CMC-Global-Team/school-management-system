efa74b53648ed5c141fef67e1e24fc41e20ad12a
dangtiendat24 <BIT230080@st.cmc-u.edu.vn>
2025-10-08 16:23:24 +0700

    update phương thức chọn trạng thái

--- src/Screen/Teacher/AddTeacherScreen.java
+++ src/Screen/Teacher/AddTeacherScreen.java
@@ -22,20 +22,56 @@ public class AddTeacherScreen extends AbstractScreen {
 
         String id;
         while (true) {
-            id = InputUtil.getNonEmptyString("Mã giáo viên: ");
+            id = InputUtil.getString("Mã giáo viên: ");
             if (teacherService.isTeacherIdExists(id)) {
                 System.out.println("Mã giáo viên đã tồn tại! Vui lòng nhập lại.");
             } else break;
         }
 
-        String name = InputUtil.getNonEmptyString("Họ và tên: ");
-        String subject = InputUtil.getNonEmptyString("Môn giảng dạy: ");
-        String degree = InputUtil.getNonEmptyString("Trình độ (Cử nhân/Thạc sĩ/Tiến sĩ): ");
+        String name = InputUtil.getString("Họ và tên: ");
+        String subject = InputUtil.getString("Môn giảng dạy (Enter nếu chưa có): ");
+
+        //Nhập trình độ theo số
+        int degreeCode = -1;
+        String degree = "";
+        while (degreeCode < 0 || degreeCode > 2) {
+            System.out.println("\nChọn trình đọ cho giáo viên:");
+            System.out.println("0 - Cử nhân");
+            System.out.println("1 - Thạc sĩ");
+            System.out.println("2 - Tiến sĩ");
+            degreeCode = InputUtil.getInt("Nhập lựa chọn (0/1/2): ");
+
+            switch (degreeCode) {
+                case 0 -> degree = "Cử nhân";
+                case 1 -> degree = "Thạc sĩ";
+                case 2 -> degree = "Tiến sĩ";
+                default -> System.out.println("Lựa chọn không hợp lệ!");
+            }
+        }
+
         int experience = InputUtil.getInt("Số năm kinh nghiệm: ");
-        String email = InputUtil.getNonEmptyString("Email: ");
-        String phone = InputUtil.getNonEmptyString("Số điện thoại: ");
+        String email = InputUtil.getString("Email: ");
+        String phone = InputUtil.getString("Số điện thoại: ");
         String homeroom = InputUtil.getString("Lớp chủ nhiệm (Enter nếu chưa có): ");
-        String status = InputUtil.getNonEmptyString("Trạng thái (Đang dạy/Nghỉ hưu/Công tác): ");
+
+        //Nhập trạng thái theo số
+        int statusCode = -1;
+        String status = "";
+        while (statusCode < 0 || statusCode > 2) {
+            System.out.println("\nChọn trạng thái giáo viên:");
+            System.out.println("0 - Đang dạy");
+            System.out.println("1 - Nghỉ hưu");
+            System.out.println("2 - Công tác");
+            statusCode = InputUtil.getInt("Nhập lựa chọn (0/1/2): ");
+
+
+            switch (statusCode) {
+                case 0 -> status = "Đang dạy";
+                case 1 -> status = "Nghỉ hưu";
+                case 2 -> status = "Công tác";
+                default -> System.out.println("Lựa chọn không hợp lệ!");
+            }
+        }
 
         boolean added = teacherService.addTeacher(
                 id, name, status, subject, degree, experience, email, phone, homeroom
